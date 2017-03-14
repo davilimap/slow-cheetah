36a421027e1cc7d7cab3ffe65556a27dfea8345f
Davi Paulino <davilimap@gmail.com>
2017-03-14 10:41:51 -0700

    Adding JSON library Adding transform class

---
+++ SlowCheetah/Transformer/JsonTransformer.cs
@@ -0,0 +1,17 @@
+﻿namespace SlowCheetah.Transformer
+{
+    using System;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Transforms JSON files according to JDT syntax
+    /// </summary>
+    public class JsonTransformer : ITransformer
+    {
+        /// <inheritdoc/>
+        public bool Transform(string source, string transform, string destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
