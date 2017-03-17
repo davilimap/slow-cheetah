c20e1b5067637cff61cb2a3901af91377c31af96
Davi Paulino <davilimap@gmail.com>
2017-03-17 14:24:36 -0700

    Streamline tests

--- SlowCheetah.Tests/JDT/DefaultTransformTest.cs
+++ SlowCheetah.Tests/JDT/DefaultTransformTest.cs
@@ -5,6 +5,7 @@ namespace SlowCheetah.Tests.JDT
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -16,55 +17,39 @@ namespace SlowCheetah.Tests.JDT
     /// <summary>
     /// Test class for JDT default transforms
     /// </summary>
-    public class DefaultTransformTest : BaseJdtTest
+    public class DefaultTransformTest
     {
-        [Fact]
-        public void PrimitiveAddition()
+        // Directory for test inputs, that are JSON files
+        private static readonly string TestInputDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\SlowCheetah.Tests\\JDT\\Inputs\\";
+
+        public static IEnumerable<object[]> GetInputs()
         {
-            this.BaseTransformTest(
-                JdtTestUtilities.GetDocumentObjectWithPrimitives,
-                JdtTestUtilities.GetTransformWithPrimitiveAdds,
-                JdtTestUtilities.GetExpectedWithPrimitiveAdds);
+            // Each transform file in the test input folder will correspond to one test
+            foreach (string file in Directory.GetFiles(TestInputDirectory, "*.Transform.json"))
+            {
+                // Transform files are called {TestCategory}.{TestName}.Transform.json
+                // Expected results files are called {TestCategory}.{TestName}.Expected.json
+                // Source files are called {TestCategory}.Source.json
+                // This returns {TestCategory}.{TestName} so the test can find the files
+                yield return new object[] { Path.GetFileNameWithoutExtension(Path.GetFileNameWithoutExtension(file)) };
+            }
         }
 
-        [Fact]
-        public void ObjectAddition()
+        [Theory]
+        [MemberData(nameof(GetInputs))]
+        public void TransformTest(string testFileName)
         {
-            this.BaseTransformTest(
-                JdtTestUtilities.GetDocumentObjectWithPrimitives,
-                JdtTestUtilities.GetTransformWithObjectAdds,
-                JdtTestUtilities.GetExpectedWithObjectAdds);
-        }
+            // Removes the test name to find the source file
+            string sourceFileName = Path.GetFileNameWithoutExtension(testFileName);
+            JsonDocument source = new JsonDocument(TestInputDirectory + sourceFileName + ".Source.json");
 
-        [Fact]
-        public void PrimitiveSubstitution()
-        {
-            this.BaseTransformTest(
-                JdtTestUtilities.GetDocumentObjectWithPrimitives,
-                JdtTestUtilities.GetTransformWithPrimitiveReplace,
-                JdtTestUtilities.GetTransformWithPrimitiveReplace);
-        }
+            JsonTransformation transformation = new JsonTransformation(TestInputDirectory + testFileName + ".Transform.json");
 
-        [Fact]
-        public void ObjectMerge()
-        {
-            this.BaseTransformTest(
-                JdtTestUtilities.GetDocumentWithObjects,
-                JdtTestUtilities.GetTransformWithObjectMerge,
-                JdtTestUtilities.GetExpectedWithObjectMerge);
-        }
+            JsonDocument expected = new JsonDocument(TestInputDirectory + testFileName + ".Expected.json");
 
-        public void ArrayMerge()
-        {
-        }
+            transformation.Apply(source);
 
-        public void LoadFromFile()
-        {
-
-        }
-
-        public void SaveToFile()
-        {
+            Assert.True(source.Equals(expected));
         }
     }
 }
